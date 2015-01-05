e1adbad864579db87876b040a5d68c78b62daf9c
hughest8 <tomrhughes24@gmail.com>
2015-01-05 17:34:42 +0000

    Update ProjectTwo.py

--- ProjectTwo.py
+++ ProjectTwo.py
@@ -7,3 +7,34 @@ canvas.pack()
 
 
 main.mainloop()
+
+'''def negcounter():
+        #global p
+        #if p==True:
+         #   global counters
+          #  counters=counters-1 #subtracts 1 from the variable counter
+           # label.after(1000,negcounter) #after waiting 1000ms the function negcounter is run  
+        
+    #def counts():
+     #   global counters
+      #  counters==counters #assigns the value of counter to the variable counter ( this was required as a function wont run unless the value of the variable is assigned)
+       # global RoboFinished
+        #RoboFinished=RoboFinished #assigns the value of RoboFinished to the variable RoboFinished ( this was required as a function wont run unless the value of the variable is assigned)
+        #if RoboFinished!=True: #count function will only run if the value for RoboFinished is False
+         #   counters=counters+1 #adds 1 to the count
+          #  label.config(text=str(counters)) #assigns the label the vaiable counter which is then displayed as a string
+            
+           # label.after(1000,counts) #the after 1000ms the label will call the count function
+        #else:
+         #   cstop() #stops the counter when RoboFinished is true
+            
+    #def counter_label(label):
+     #   global counters
+      #  counters = 0 #sets the starting value of counter at 0
+       # if counters!=100000000: #calls the  function count() when the value of counter does not equal 100000000
+        #    counts()
+            
+    #def cstop():
+     #   print ("The Robot Reached The End") #prints a message stating that the robot has finished
+      #  tkMessageBox.showinfo("Counter Terminated, Counted to:",counters) #displays a messagebox stating that the robot finished in x seconds
+'''
