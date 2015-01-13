9aa40bf900d03ce918b85adf9c6c8cd6bd63cdd7
Ash F <Ashtia.f@hotmail.co.uk>
2015-01-13 17:02:32 +0000

    Update Level 1.py
    Just added the graph with the search algorithm. It finds the route to the end destination, not the shortest route, but it will still get the route to the end destination.

--- Level 1.py
+++ Level 1.py
@@ -3,15 +3,21 @@ import random
 main = Tk(className = "Level 1")
 canvas = Canvas(main, width = 1280, height = 720, bg = "Black")
 canvas.pack()
-def DFS(route, start): #Graph and start node as arguments
+
+def DFS(route, start, end): #Graph and start node as arguments
     path=[] #List of nodes in the path 
     queue=[start] #Queue list 
     while queue: #While list is not empty
         v=queue.pop(0) #Remove node from list
+        if v == end:
+            return path
         if v not in path: #If node v has not been checked yet
             path=path+[v] #Add node to path list
             queue=route[v]+queue #Add node's neighbors at the beginning of the list 
     return path
+
+
+
 class objects:
 
     def __init__(self,x,y,length,width,colour,TreasurePresent,canvas):
@@ -24,70 +30,19 @@ class objects:
         self.canvas=canvas
         self.object = canvas.create_rectangle(self.x,self.y,self.x+self.length,self.y+self.width,fill = self.colour)
 
-class interface:
+class light:
 
-    def __init__(self, name):
-        self.start_button = Button(name, text="Start", width = 20, command=self.start, bg = "Green")
-        self.start_button.place(x = 1110, y = 150)
+    def __init__(self,x0,y0,x1,y1,colour):
+        self.x0 = x0
+        self.y0 = y0
+        self.x1 = x1
+        self.y1 = y1
+        self.colour = colour
+        self.object = canvas.create_oval(self.x0,self.y0,self.x1,self.y1,fill = self.colour)
 
-        self.pause_button = Button(name, text = "Pause", width = 20, command = self.pause, bg = "Light Blue")
-        self.pause_button.place(x = 1110, y = 200)
 
-        self.reset_button = Button(name, text="Reset", width = 20, command=self.reset, bg = "Orange")
-        self.reset_button.place(x = 1110, y = 250)
 
-        self.nextLevel_button = Button(name, text="Next Level", width = 20, command=self.nextLevel, bg = "Yellow")
-        self.nextLevel_button.place(x = 1110, y = 300)
 
-        self.timerShow_label = Label(name, text = "", width = 7, font = ("Arial", 16))
-        self.timerShow_label.place(x = 1170, y = 30)
-
-        self.timer_label= Label(name,text ="Timer", width = 5, font = ("Arial", 16))
-        self.timer_label.place(x = 1110, y = 30)
-
-        self.treasures_label = Label(name, text = "Treasure Remaining: ", width = 16, height = 2, font = ("Arial", 12), anchor = N)
-        self.treasures_label.place(x = 1110, y = 70)
-
-        self.treasureShow_label = Label(name, text = "1", width = 16, font = ("Arial", 12))
-        self.treasureShow_label.place(x = 1110, y = 100)
-
-    def start(self):
-        print "Start"
-
-    def pause(self):
-        print "Pause"
-
-    def reset(self):
-        print "Reset"
-
-    def nextLevel(self):
-        print "Next Level"
-
-    def count(main):
-        global counter, resetpressed, pausepressed
-        counter==counter
-        count=0
-        global RoboFinished
-        RoboFinished==RoboFinished
-        if RoboFinished !=True:
-            counter=counter+1
-            print "Checkpoint"
-            main.timerShow_label.config(text = str(counter))
-            main.timerShow_label.after(1000, main.count) 
-        elif resetpressed==True:
-            print "Wololol"
-        elif pausepressed==True:
-            print "Wololol 2"
-        else:
-            cstop()
-
-    def counter_label(main,self):
-        
-            global counter, RoboFinished
-            counter=0
-            RoboFinished=False
-            if counter!=1000000:
-                interface.count()
 class Road:
     def __init__(self,name,x,y,width,height):
         self.name=name
@@ -139,10 +94,26 @@ class Robot:
         
         route = {'Road1':['B', 'C'], 'B':['E'], 'C':['E', 'D'], 'D':['E'], 'E':['F'], 'F':[]} #Connectivity 
         print 'DFS path: ', DFS(route, self.CheckPosition())
-interface = interface(main)
-interface = interface.counter_label(interface)
+
+'''class button:
+
+    def __init__(self, name, text, width, command, bg, x, y):
+        self.name = name
+        self.text = text
+        self.width = width
+        self.command = command
+        self.bg = bg
+        self.x = x
+        self.y = y
+        self.canvas = canvas
+        self.button = Button(self.name, self.text, self.width, self.command, self.bg)
+        self.button.place = (self.x, self.y)
         
-#Roads
+        
+    def test1(self):
+        print "test1"
+
+button1 = button(main,"Test", 10, button.test1, "Green", 10, 10)'''
 Road1=Road('Road1',10,45,1070,40)
 Road2=Road('Road2',10,45,40,630)
 Road3=Road('Road3',1040,45,40,480)
@@ -173,7 +144,7 @@ pave1 = objects(10.0,10.0,1070.0,35.0, "Light Grey",False,canvas)
 object1 = objects(10.0,15.0,200.0, 25.0, "Red",False,canvas)
 object2 = objects(290.0,15.0,200.0,25.0, "Red",False,canvas)
 object3 = objects(580.0,15.0,200.0,25.0, "Red",False,canvas)
-object4 = objects(870.0,15.0,210.0,25.0, "Red",True,canvas)
+object4 = objects(870.0,15.0,210.0,25.0, "Red",False,canvas)
 
 #second row
 pave2 = objects(50.0,85.0,354.0,35.0, "Light Grey", False,canvas)
@@ -184,8 +155,8 @@ pave4 = objects(519.0,85.0,351.0,35.0,"Light Grey",False,canvas)
 object7 = objects(524.0,90.0,341.0,25.0,"Red",False,canvas)
 pave5 = objects(910.0,85.0,35.0,150.0,"Light Grey",False,canvas)
 object8 = objects(915.0,90.0,25.0,140.0,"Red",False,canvas)
-pave6 = objects(985.0,85.0,35.0,110.0,"Light Grey",False,canvas)
-object9 = objects(990.0,90.0,25.0,100.0, "Red",False,canvas)
+pave6 = objects(985.0,85.0,55.0,110.0,"Light Grey",False,canvas)
+object9 = objects(990.0,90.0,45.0,100.0, "Red",False,canvas)
 
 #Third Row
 pave7 = objects(50.0,160.0,180.25,35.0,"Light Grey", False,canvas)
@@ -193,15 +164,25 @@ object10 = objects(55.0,165.0,170.0,25.0,"Red", False,canvas)
 pave8 = objects(271.25,160,172.75,35.0, "Light Grey", False,canvas)
 object11 = objects(276.25,165.0,162.75,25.0, "Red", False, canvas)
 pave9 = objects(519.0,160.0,351.0,35.0,"Light Grey",False,canvas)
-object11 = objects(524.0,165.0,341.0,25.0,"Red",False,canvas)
+object12 = objects(524.0,165.0,341.0,25.0,"Red",False,canvas)
 
 #Fourth Row
-pave10 = objects(50.0,235.0,180.25,250.0, "Light Grey", False,canvas)
-object12 = objects(55.0,240.0,170.25,240.0, "Red", False,canvas)
-
-
+pave10 = objects(50.0,235.0,180.25,105.0, "Light Grey", False,canvas)
+object13 = objects(55.0,240.0,170.25,95.0, "Red", False,canvas)
+pave11 = objects(270.25, 235.0, 180.25,105.0, "Light Grey", False, canvas)
+object14 = objects(275.0, 240.0,170.0,95.0, "Red", False, canvas)
+pave12 = objects(490.25,235.0,549.75,105.0, "Light Grey", False, canvas)
+object15 = objects(495.25,240.0,539.75,95.0, "Red", False, canvas)
 
 #Fifth Row
+pave21 = objects(50.0,380.0,180.25,105.0, "Light Grey", False,canvas)
+object27 = objects(55.0,385.0,170.25,95.0, "Red", False,canvas)
+pave22 = objects(270.25, 380.0, 180.25,105.0, "Light Grey", False, canvas)
+object28 = objects(275.0, 385.0,170.0,95.0, "Red", False, canvas)
+pave23 = objects(490.25,380.0,549.75,105.0, "Light Grey", False, canvas)
+object29 = objects(495.25,385.0,539.75,95.0, "Red", False, canvas)
+
+#Sixth Row
 pave13 = objects(50.0,525.0,276.5,35.0, "Light Grey", False,canvas)
 object16 = objects(55.0,530.0,266.5,25.0, "Red", False,canvas)
 pave14 = objects(366.0,525.0,351.0,35.0, "Light Grey", False,canvas)
@@ -211,27 +192,35 @@ object18 = objects(762.0,530.0,143.0,25.0,"Red",False,canvas)
 pave16 = objects(910.0,525.0,170.0,150.0, "Light Grey", False,canvas)
 object19 = objects(915.0,530.0,160.0,140.0, "Red", True, canvas)
 
-#Sixth Row
+#Seventh Row
 pave17 = objects(50.0,600.0,180.25,35.0,"Light Grey", False,canvas)
-object20 = objects(55.0,605.0,170.0,25.0, "Red", True, canvas)
+object20 = objects(55.0,605.0,170.0,25.0, "Red", False, canvas)
 pave18 = objects(271.25,600.0,207.75,35.0, "Light Grey", False,canvas)
 object21 = objects(276.25,605.0,197.75,25.0, "Red", False, canvas)
 pave19 = objects(519.0,600.0,351.0,35.0,"Light Grey", False,canvas)
 object22 = objects(524,605,341.0,25.0, "Red", False,canvas)
 
-#Seventh Row
+#Eighth Row
 pave20 = objects(10.0,675.0,1070.0,35.0, "Light Grey",False,canvas)
 object23 = objects(10.0,680.0,200.0, 25.0, "Red",False,canvas)
 object24 = objects(290.0,680.0,200.0,25.0, "Red",False,canvas)
 object25 = objects(580.0,680.0,200.0,25.0, "Red",False,canvas)
-object26 = objects(870.0,680.0,210.0,25.0, "Red",True,canvas)
+object26 = objects(870.0,680.0,210.0,25.0, "Red",False,canvas)
 
+ListOfLandmarks = (object4, object13,object19,object20)
+'''r2d2 = Robot(0, 0, speed = 1, colour='yellow')
 c3po = Robot(0, 0, speed = 1, size=20, colour='cyan')
+r2d2.RandomPosition()
 c3po.RandomPosition()
+r2d2.drawRobot() 
 c3po.drawRobot()
-#c3po.Move()
+c3po.Move()'''
+'''Light1 = lights(20.0,130.0,40,150,"Green")
+Light2 = lights(20.0,205.0,40,225,"Green")'''
+
+route1 = {Road1:[Road2, Road9, Road7, Road4, Road3], Road2:[Road1,Road10,Road5,Road13,Road14,Road16, Road19], Road3: [Road1,Road5,Road13,Road14], Road4:[Road1,Road5], Road5:[Road3, Road4], Road6:[Road2,Road11,Road8,Road15,Road7], Road7:[Road6,Road12,Road1], Road8:[Road1,Road12,Road6], Road9:[Road1,Road10], Road10:[Road9,Road2,Road11], Road11:[Road10,Road6,Road13, Road14], Road12:[Road8,Road7], Road13:[Road2,Road11,Road15,Road3], Road14:[Road2,Road17,Road15,Road18, Road3], Road15:[Road6,Road13,Road14], Road16:[Road2,Road20,Road17,Road21,Road8,Road22], Road17:[Road14,Road16], Road18:[Road14,Road16], Road19:[Road19,Road20,Road21,Road22], Road20:[Road16,Road19], Road21: [Road16,Road19], Road22: [Road16,Road19]}
+print DFS(route1, Road21, Road1)
 
 
 
 main.mainloop()
-
