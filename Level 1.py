02c8f9715e1ca9619dd450d918b42a8bdad9a02b
Cameron William Gough <goughc4@uni.coventry.ac.uk>
2015-01-14 11:09:20 +0000

    Moved Objects

--- Level 1.py
+++ Level 1.py
@@ -163,20 +163,20 @@ pave9 = objects(519.0,160.0,351.0,35.0,"Light Grey",False,canvas)
 object12 = objects(524.0,165.0,341.0,25.0,"Red",False,canvas)
 
 #Fourth Row
-pave10 = objects(50.0,235.0,180.25,105.0, "Light Grey", False,canvas)
-object13 = objects(55.0,240.0,170.25,95.0, "Red", False,canvas)
-pave11 = objects(270.25, 235.0, 180.25,105.0, "Light Grey", False, canvas)
-object14 = objects(275.0, 240.0,170.0,95.0, "Red", False, canvas)
-pave12 = objects(490.25,235.0,549.75,105.0, "Light Grey", False, canvas)
-object15 = objects(495.25,240.0,539.75,95.0, "Red", False, canvas)
+pave10 = objects(50.0,235.0,180.25,105.0, "Light Grey", canvas)
+object13 = landmarks(55.0,240.0,170.25,95.0, "Blue", canvas)
+pave11 = objects(270.25, 235.0, 180.25,105.0, "Light Grey",canvas)
+object14 = objects(275.0, 240.0,170.0,95.0, "Red",canvas)
+pave12 = objects(490.25,235.0,549.75,105.0, "Light Grey",canvas)
+object15 = objects(495.25,240.0,539.75,95.0, "Red",canvas)
 
 #Fifth Row
-pave21 = objects(50.0,380.0,180.25,105.0, "Light Grey", False,canvas)
-object27 = objects(55.0,385.0,170.25,95.0, "Red", False,canvas)
-pave22 = objects(270.25, 380.0, 180.25,105.0, "Light Grey", False, canvas)
-object28 = objects(275.0, 385.0,170.0,95.0, "Red", False, canvas)
-pave23 = objects(490.25,380.0,549.75,105.0, "Light Grey", False, canvas)
-object29 = objects(495.25,385.0,539.75,95.0, "Red", False, canvas)
+pave21 = objects(50.0,380.0,180.25,105.0, "Light Grey",canvas)
+object27 = objects(55.0,385.0,170.25,95.0, "Red", canvas)
+pave22 = objects(270.25, 380.0, 180.25,105.0, "Light Grey",canvas)
+object28 = objects(275.0, 385.0,170.0,95.0, "Red",canvas)
+pave23 = objects(490.25,380.0,549.75,105.0, "Light Grey",canvas)
+object29 = landmarks(495.25,385.0,539.75,95.0, "Blue",canvas)
 
 #Sixth Row
 pave13 = objects(50.0,525.0,276.5,35.0, "Light Grey", False,canvas)
