7718175363a9b1375669c4ab59b1d5d0d0b2c3f1
Thomas Holmes <tmholmes15@gmail.com>
2015-01-20 17:18:34 +0000

    Update Level 1.py
    Worked on by Alex Hodson and Tom Holmes - All Landmarks added

--- Level 1.py
+++ Level 1.py
@@ -353,8 +353,11 @@ object25 = objects(580.0,680.0,200.0,25.0, "Red",canvas)
 object26 = objects(870.0,680.0,210.0,25.0, "Red",canvas)
 
 #Landmarks
-Landmark1 = Landmarks(55.0,67.0,10.0,20.0,"blue",canvas,"Dave",True) 
-
+Landmark1 = Landmarks(55.0,67.0,10.0,20.0,"blue",canvas,"Dave",True)
+Landmark2 = Landmarks(200.0,583.0,10.0,20.0,"blue",canvas,"Jason",False)
+Landmark3 = Landmarks(383.0,508.0,10.0,20.0,"blue",canvas,"Kate",False)
+Landmark4 = Landmarks(860.25,363.0,10.0,20.0,"blue",canvas,"Matt",False)
+Landmark5 = Landmarks(990.0,67.0,10.0,20.0,"blue",canvas,"Phil",False)
 #Treasures
 Treasure1 = Treasure(55.0,62.0,10.0,5.0,"dark green",canvas,False,100)
 
